396ea7f969c5ace9cdc91dba4d91556fd2780df6
shaheer-cowlar <shaheer.khan@cowlar.com>
2023-07-10 16:59:14 +0500

    added put post and get requests

--- index.js
+++ index.js
@@ -1,37 +1,26 @@
 const express = require('express');
 const path = require('path');
 const members = require('./Members');
+const { use } = require('./routes/api/members');
+
 
 const app = express();
 
+// const logger = (req, res , next) => {
+//     console.log('Hello');
+//     console.log(req.protocol+'://'+req.get('host')+req.originalUrl) ;
+//     next();
+// };
 
+// app.use(logger);
 
-// app.get('/',(req,res)=>{
+//body parser middleware
+app.use(express.json ());
+app.use(express.urlencoded({extended:false}));
 
-//     res.sendFile(path.join(__dirname,'public','index.html'));
-
-// })
-
-const logger = (req, res , next) => {
-    console.log('Hello');
-    console.log(req.protocol+'://'+req.get('host')+req.originalUrl);
-    next();
-};
-
-app.use(logger);
-
-app.get('/api/members',(req,res)=>{
-res.json(members);
-});
-
+//set static folder
 app.use(express.static(path.join(__dirname,'public',)));
-
-
-
+app.use('/api/members',require('./routes/api/members'));
 
 const PORT = process.env.PORT || 5000;
-
-
-
-
 app.listen(PORT,()=>console.log('Server started on port'+ PORT));
---
+++ routes/api/members.js
@@ -0,0 +1,68 @@
+const members = require('../../Members');
+const express = require('express');
+const router = express.Router();
+const uuid = require('uuid');
+
+
+
+//get  member
+router.get('/',(req,res)=>{
+    res.json(members);
+    });
+    
+//get single members
+router.get('/:id',(req,res)=>{
+        //res.send(req.params.id);
+        const found = members.some(member=>member.id===parseInt(req.params.id));
+        if(found){
+            res.json(members.filter(member=>member.id===parseInt(req.params.id)));
+        }
+        else{
+            res.status(400).json({msg:'No member with id of ' + req.params.id});
+        }
+        
+    });
+
+//create member
+router.post('/',(req,res)=>{
+    const newMember = 
+    {
+        id: uuid.v4(),
+        name:req.body.name,
+        email:req.body.email,
+        status:'active'
+    };
+    if(!newMember.name||!newMember.email){
+        return res.status(400).json({msg:'please include name and email'});
+    }
+
+    members.push(newMember);
+    res.json(members);
+
+});
+
+//Update member
+
+router.put('/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id));
+
+    if (found) {
+        const updMember = req.body;
+
+        members.forEach(member => {
+            if (member.id === parseInt(req.params.id)) {
+                member.name = updMember.name ? updMember.name : member.name;
+                member.email = updMember.email ? updMember.email : member.email;
+
+                res.json({ msg: 'Member updated', member: member });
+            }
+        });
+    } else {
+        res.status(400).json({ msg: 'No member with id of ' + req.params.id });
+    }
+});
+
+
+
+
+module.exports = router;
